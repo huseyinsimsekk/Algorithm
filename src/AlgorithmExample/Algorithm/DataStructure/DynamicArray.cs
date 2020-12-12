95fc1453b37ddd1909dc1e899871be2005ed6357
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-12-12 16:30:37 +0300

    DynamicArray

---
+++ src/AlgorithmExample/Algorithm/DataStructure/DynamicArray.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.DataStructure
+{
+    public class DynamicArray<T>
+    {
+        int Size;
+        Object[] Data;
+        public DynamicArray()
+        {
+            Size = 0;
+            Data = new Object[1];
+        }
+        public int GetSize()
+        {
+            return Data.Length;
+        }
+        public T GetT(int index)
+        {
+            return (T)Data[index];
+        }
+        public void Add(Object data)
+        {
+            EnsureCapacity(Size + 1);
+            Data[Size++] = data;
+        }
+        public void EnsureCapacity(int minCapacity)
+        {
+            int oldCapacity = GetSize();
+            if (minCapacity > oldCapacity)
+            {
+                int newCapacity = oldCapacity * 2;
+                if (newCapacity < minCapacity)
+                {
+                    newCapacity = minCapacity;
+                }
+                Array.Resize(ref Data, newCapacity);              
+            }
+        }
+    }
+}
--- src/AlgorithmExample/Algorithm/Program.cs
+++ src/AlgorithmExample/Algorithm/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Algorithm.DataStructure;
+using System;
 
 namespace Algorithm
 {
@@ -6,7 +7,23 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var data = new DynamicArray<int>();
+            Console.WriteLine(data.GetSize());  // 1
+            data.Add(1);
+            Console.WriteLine(data.GetSize()); // 1
+            data.Add(2);
+            Console.WriteLine(data.GetSize()); // 2
+            data.Add(3);
+            Console.WriteLine(data.GetSize()); // 4
+            data.Add(4);
+            Console.WriteLine(data.GetSize()); // 4
+            data.Add(4);
+            Console.WriteLine(data.GetSize()); // 8
+            Console.WriteLine("*********************");
+            for (int i = 0; i < 4; i++)
+            {
+            Console.WriteLine(data.GetT(i));
+            }
         }
     }
 }
