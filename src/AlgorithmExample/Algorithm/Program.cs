e5af3a2aa6abf6d85284a31440a712d65872897c
huseyinsimsekk <hsynsimsek723@gmail.com>
2020-12-13 14:08:29 +0300

    Delete Kth element from linkedlist method added

--- src/AlgorithmExample/Algorithm/Program.cs
+++ src/AlgorithmExample/Algorithm/Program.cs
@@ -7,32 +7,56 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            //var data = new DynamicArray<int>();
-            //Console.WriteLine(data.GetSize());  // 1
-            //data.Add(1);
-            //Console.WriteLine(data.GetSize()); // 1
-            //data.Add(2);
-            //Console.WriteLine(data.GetSize()); // 2
-            //data.Add(3);
-            //Console.WriteLine(data.GetSize()); // 4
-            //data.Add(4);
-            //Console.WriteLine(data.GetSize()); // 4
-            //data.Add("fdddf");
-            //Console.WriteLine(data.GetSize()); // 8
-            //Console.WriteLine("*********************");
-            //for (int i = 0; i < data.GetSize(); i++)
-            //{
-            //    Console.WriteLine(data.GetT(i));
-            //}
+            var data = new DynamicArray<int>();
+            Console.WriteLine(data.GetSize());  // 1
+            data.Add(1);
+            Console.WriteLine(data.GetSize()); // 1
+            data.Add(2);
+            Console.WriteLine(data.GetSize()); // 2
+            data.Add(3);
+            Console.WriteLine(data.GetSize()); // 4
+            data.Add(4);
+            Console.WriteLine(data.GetSize()); // 4
+            data.Add("fdddf");
+            Console.WriteLine(data.GetSize()); // 8
+            Console.WriteLine("*********************");
+            for (int i = 0; i < data.GetSize(); i++)
+            {
+                Console.WriteLine(data.GetT(i));
+            }
 
-            var linkedList = new LinkedList<string>();
-            linkedList.Add("huseyin");
-            linkedList.Add("şimşek");
+            var linkedList = new LinkedList<int>();
+            linkedList.Add(1);
+            linkedList.Add(2);
+            linkedList.Add(3);
+            linkedList.Add(4);
+            linkedList.Add(5);
+            linkedList.Add(6);
             Console.WriteLine("*************");
             linkedList.Display();
+
+            DeleteKthElement(linkedList.head, 2);
             Console.WriteLine("*************");
-            linkedList.Delete();
             linkedList.Display();
+        }
+        static void DeleteKthElement(Node root, int k) // O(n)
+        {
+            // init pointers
+            Node slowPointer = root;
+            Node fastPointer = root;
+
+            // move fastPointer to slowPointer + k
+            while (k > 0)
+            {
+                fastPointer = fastPointer.next;
+                k--;
+            }
+            while (fastPointer.next != null)
+            {
+                slowPointer = slowPointer.next;
+                fastPointer = fastPointer.next;
+            }
+            slowPointer.next = slowPointer.next.next;
         }
     }
 }
