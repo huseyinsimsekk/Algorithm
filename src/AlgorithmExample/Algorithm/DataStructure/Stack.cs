9fe4b4f0f6163fc1a5bea394030124f735e78605
huseyinsimsekk <hsynsimsek723@gmail.com>
2021-01-02 10:51:09 +0300

    Stack added

---
+++ src/AlgorithmExample/Algorithm/DataStructure/Stack.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Algorithm.DataStructure
+{
+    public class Stack
+    {
+        private Object[] items;
+        private int top;
+        private int size;
+        public Stack(int size)
+        {
+            this.size = size;
+            items = new Object[size];
+            top = -1;
+        }
+        public void Push(Object item)
+        {
+            top = top + 1;
+            if (IsFull())
+            {
+                Console.WriteLine("Stack is overflow");
+                return;
+            }
+            items[top] = item;
+
+        }
+        public Object Pop()
+        {
+            top = top - 1;
+            if (IsEmpty())
+            {
+                Console.WriteLine("Stack is empty");
+                return null;
+            }
+            var popItem = items[top];
+            return popItem;
+        }
+        private bool IsEmpty()
+        {
+            return top <= -1;
+        }
+        private bool IsFull()
+        {
+            return size == top;
+        }
+    }
+}
--- src/AlgorithmExample/Algorithm/Program.cs
+++ src/AlgorithmExample/Algorithm/Program.cs
@@ -7,36 +7,7 @@ namespace Algorithm
     {
         static void Main(string[] args)
         {
-            var data = new DynamicArray<int>();
-            Console.WriteLine(data.GetSize());  // 1
-            data.Add(1);
-            Console.WriteLine(data.GetSize()); // 1
-            data.Add(2);
-            Console.WriteLine(data.GetSize()); // 2
-            data.Add(3);
-            Console.WriteLine(data.GetSize()); // 4
-            data.Add(4);
-          
-            Console.WriteLine(data.GetSize()); // 8
-            Console.WriteLine("*********************");
-            for (int i = 0; i < data.GetSize(); i++)
-            {
-                Console.WriteLine(data.GetT(i).ToString());
-            }
-
-            var linkedList = new LinkedList<int>();
-            linkedList.Add(1);
-            linkedList.Add(2);
-            linkedList.Add(3);
-            linkedList.Add(4);
-            linkedList.Add(5);
-            linkedList.Add(6);
-            Console.WriteLine("*************");
-            linkedList.Display();
-
-            DeleteKthElement(linkedList.head, 6);
-            Console.WriteLine("*************");
-            linkedList.Display();
+            Console.WriteLine("Hello World");
         }
         static void DeleteKthElement(Node root, int k) // O(n)
         {
